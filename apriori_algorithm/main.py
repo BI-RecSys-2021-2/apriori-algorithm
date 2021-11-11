ecee3446b5f1ed5f5cfa1bb11de9c4e2dbc7019a
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-11-11 18:18:23 +0900

    apriori algorithm and association rules

--- apriori_algorithm/main.py
+++ apriori_algorithm/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-pd.set_option('display.max_rows', 30)
+pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 200)
 pd.set_option('display.width', 1000)
 
@@ -11,4 +11,8 @@ df = pd.read_csv("./data/input/groceries_categorized.csv")
 #One hot encoding
 oh = pd.get_dummies(df.drop(columns=['Item(s)'], axis=1), prefix='', prefix_sep='')
 oh = oh.groupby(level=0, axis=1).sum()
-print(oh)
+
+fi = apriori(oh, min_support=0.03, use_colnames=True)
+
+rules = association_rules(fi, metric="lift", min_threshold=1)
+print(rules.head(100))
