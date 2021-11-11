65ded9bb089936d977e026f1a875c8eeac01b3c5
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-11-11 19:08:10 +0900

    export result to csv (min Support 0.02, min Lift 1)

--- apriori_algorithm/main.py
+++ apriori_algorithm/main.py
@@ -13,10 +13,10 @@ df = pd.read_csv("./data/input/groceries_categorized.csv")
 oh = pd.get_dummies(df.drop(columns=['Item(s)'], axis=1), prefix='', prefix_sep='')
 oh = oh.groupby(level=0, axis=1).sum()
 
-fi = apriori(oh, min_support=0.03, use_colnames=True)
+fi = apriori(oh, min_support=0.02, use_colnames=True)
 
 rules = association_rules(fi, metric="lift", min_threshold=1)
 print(rules)
 
-outputPath = r'/Users/yejoolee/Library/Mobile Documents/com~apple~CloudDocs/2021-2/비즈니스인텔리전스/팀플/backend/apriori_algorithm/data/output/minS_03_minL_1.csv'
+outputPath = r'/Users/yejoolee/Library/Mobile Documents/com~apple~CloudDocs/2021-2/비즈니스인텔리전스/팀플/backend/apriori_algorithm/data/output/minS_02_minL_1.csv'
 rules.to_csv(outputPath, index=True)
