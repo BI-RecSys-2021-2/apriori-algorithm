5572fdb4418df8238d74c4abb89691aff4765681
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-11-11 16:27:37 +0900

    adjust one hot encoding

--- apriori_algorithm/main.py
+++ apriori_algorithm/main.py
@@ -1,14 +1,14 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
-# for dirname, _, filenames in os.walk('./data/input'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
+pd.set_option('display.max_rows', 30)
+pd.set_option('display.max_columns', 200)
+pd.set_option('display.width', 1000)
 
 df = pd.read_csv("./data/input/groceries_categorized.csv")
-df.head(10)
-df.describe()
-df.info()
+
+#One hot encoding
+oh = pd.get_dummies(df.drop(columns=['Item(s)'], axis=1), prefix='', prefix_sep='')
+oh = oh.groupby(level=0, axis=1).sum()
+print(oh)
