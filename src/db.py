a7a5d7870192560b74e0d4237b1cb3833896e02d
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-11-19 19:13:24 +0900

    define function for the searching products among the priority

--- src/db.py
+++ src/db.py
@@ -1,6 +1,6 @@
 import pymysql
 
-db = pymysql.connect(host='175.210.42.85', port=3306, user='root', password='wangzp043640', db='text', charset='utf8')
+db = pymysql.connect(host='175.210.42.85', port=3306, user='root', password='root123', db='text', charset='utf8')
 
 cursor = db.cursor(pymysql.cursors.DictCursor)
 sql = "select version()"
--- src/search_product.py
+++ src/search_product.py
@@ -4,13 +4,28 @@ pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 200)
 pd.set_option('display.width', 1000)
 
-df = pd.read_csv("../apriori_algorithm/data/output/minS_01_minL_1.csv")
+apriori = pd.read_csv("../apriori_algorithm/data/output/minS_01_minL_1.csv")
+prod = pd.read_csv("./data/dataymx.csv", encoding='cp949')
 
 def search_product(antecedent):
-    condition = (df.antecedents == 'beef')
-    result = df[condition]
+    condition = (apriori.antecedents == antecedent)
+    result = apriori[condition]
     result = result.sort_values('lift', ascending=False)
+
+    products = result['consequents'].values.tolist()
+    print(result)
+    print(products)
+
+    return products
+
+def priority(consequents):
+    condition = prod['search'].isin(consequents)
+    result = prod[condition]
+    result = result.sort_values('freshness')
 
     print(result)
 
-search_product('beef')
+    return result
+
+consequents = search_product('bottled water')
+priority(consequents)
